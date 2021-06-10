ad30d6b00bb63f5240fa041e8ca554f32538a599
alambrow <alexander.lambrow@gmail.com>
2021-06-10 13:39:21 -0500

    geolocation working

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
 let map, infoWindow;
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
+  // map = new google.maps.Map(document.getElementById("map"), {
+  //   center: { lat: 36.15470, lng: -86.78616 },
+  //   zoom: 14.5,
+  // });
+
+  const localContextMapView = new google.maps.localContext.LocalContextMapView({
+    element: document.getElementById("map"),
+    placeTypePreferences: [
+      { type: "restaurant" },
+      { type: "tourist_attraction" },
+    ],
+    maxPlaceCount: 12,
+  });
+  map = localContextMapView.map;
+  map.setOptions({
     center: { lat: 36.15470, lng: -86.78616 },
-    zoom: 14.5,
+    zoom: 14,
   });
 
   infoWindow = new google.maps.InfoWindow();
 
-  // const locationButton = document.createElement("button");
-  // locationButton.textContent = "Pan to Current Location";
-  // locationButton.classList.add("custom-map-control-button");
-  // map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+//   // const locationButton = document.createElement("button");
+//   // locationButton.textContent = "Pan to Current Location";
+//   // locationButton.classList.add("custom-map-control-button");
+//   // map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
 
-
-  // locationButton.addEventListener("click", () => {
-    // Try HTML5 geolocation.
+//     // Try HTML5 geolocation.
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         (position) => {
@@ -36,7 +48,7 @@ function initMap() {
       // Browser doesn't support Geolocation
       handleLocationError(false, infoWindow, map.getCenter());
     }
-  // });
+  
 }
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
