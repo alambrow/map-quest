8b3a96d36917afaaf366eb4bcd52d975356a3cb1
alambrow <alexander.lambrow@gmail.com>
2021-06-11 10:52:40 -0500

    working

---
+++ dataprovider.js
@@ -0,0 +1,13 @@
+// let applcationState = {}
+
+// export const fetchPlaces = () => {
+//     return fetch("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&type=restaurant&key=AIzaSyDPylV5jpvE-KR6gSprY04JvFglLMuDnME", 
+//     {mode: 'no-cors',
+//     method: "GET"})
+//     .then(
+//         (placeData) => {
+//             applcationState.places = placeData
+//         }
+//         )
+//     }
+    
---
+++ index3.js
@@ -0,0 +1,13 @@
+// import { fetchPlaces } from "./dataprovider.js"
+
+// const renderPlaces = () => {
+//   fetchPlaces().then(() => {
+//       document.querySelector("#places").innerHTML = GooglePlaces()
+//   })
+// }
+
+// renderPlaces()
+
+// const GooglePlaces = () => {
+//   return applicationState.results
+// }
